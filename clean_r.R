82cd13d3fcd11c385422807c3a6dc6224d0d5155
Paulo <hbr.paulo@gmail.com>
2022-10-22 14:49:54 -0300

    Update clean_r.R
    Delete the unwanted code to download covid data

--- clean_r.R
+++ clean_r.R
@@ -1,12 +1,6 @@
 try(dev.off(), silent = TRUE)
 
 rm(list = ls()[!ls()=="X"])
-
-if(!exists("X")){
-  X <- COVID19::covid19(country = "Brazil") %>% 
-    pull(confirmed) %>% diff %>% zoo::rollmean(k = 7)
-}
-
 gc()
 
 rstudioapi::restartSession()
