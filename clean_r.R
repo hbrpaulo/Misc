90f28c6c823fb06194d8edbb6245f0bec868c514
Paulo <hbr.paulo@gmail.com>
2024-03-18 12:26:23 -0300

    Update clean_r.R

--- clean_r.R
+++ clean_r.R
@@ -4,7 +4,7 @@ try(
 
 cat('\f') # Clean console
 
-# Remove all variable, except the ones listed in a variable called "fixed_data"
+# Remove all variables, except the ones listed in a variable called "fixed_data"
 if(exists('fixed_data')){
   rm(list = ls()[!ls() %in% c(fixed_data, "fixed_data")])
 }else{rm(list = ls())}
