7fe8ff1eda057699d91e12bfdd2e750a2f78a005
Paulo Henrique Brasil Ribeiro <hbrpaulo@usp.br>
2024-09-19 00:02:38 -0300

    teste traducao

--- finance_app/app.R
+++ finance_app/app.R
@@ -1,7 +1,7 @@
 # ui
-#source('https://raw.githubusercontent.com/hbrpaulo/Misc/main/finance_app/scripts/ui.R')
-source('scripts/ui.R')
+source('https://raw.githubusercontent.com/hbrpaulo/Misc/main/finance_app/scripts/ui.R')
+#source('scripts/ui.R')
 # server
-#source('https://raw.githubusercontent.com/hbrpaulo/Misc/main/finance_app/scripts/server.R')
-source('scripts/server.R')
+source('https://raw.githubusercontent.com/hbrpaulo/Misc/main/finance_app/scripts/server.R')
+#source('scripts/server.R')
 shinyApp(ui, server)
