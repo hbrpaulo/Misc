b379768bf8e5d2f2af0e1a7d336d2a15ea29193e
Paulo Henrique Brasil Ribeiro <hbrpaulo@usp.br>
2024-10-02 19:56:37 -0300

    Demo
    Add a shiny demo to show the function that are being produced

---
+++ CurveInterpreter/CurveIntDemo/ui.R
@@ -0,0 +1,37 @@
+library(shiny)
+library(tibble)
+library(ggplot2)
+
+# Interface do usuário
+ui <- fluidPage(
+  titlePanel("Upload e Seleção de Colunas"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      fileInput("file", "Carregue o arquivo CSV", accept = ".csv"),
+      
+      # Selecionar a variável 'res' obrigatoriamente
+      uiOutput("res_column"),
+      
+      # Opção A: Condicional de seleção da variável 'ref'
+      checkboxInput("optionA", "Selecionar uma coluna ref?", FALSE),
+      uiOutput("ref_column"),
+      
+      # Opção B: Se Opção A for selecionada, escolher opcionalmente ref_sup e ref_inf
+      conditionalPanel(
+        condition = "input.optionA == true",
+        checkboxInput("optionB", "Selecionar ref_sup e ref_inf?", FALSE),
+        uiOutput("ref_sup_column"),
+        uiOutput("ref_inf_column")
+      )
+    ),
+    
+    mainPanel(
+      tableOutput("preview"),
+      plotOutput("ggplotf")
+    )
+  )
+)
+
+
+#shinyApp(ui = ui, server = server)
