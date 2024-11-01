b66d79ac5df0f79186b258dd86e914dae573f0bf
Paulo <hbr.paulo@gmail.com>
2024-10-31 21:50:20 -0300

    Update msdr.R

--- msdr.R
+++ msdr.R
@@ -1,12 +1,13 @@
 msdr <- function(x, k = 2) {
-  # Remove NA values from input vector 'x' to avoid errors in calculations
+  
+  #' Remove NA values from input vector 'x' to avoid errors in calculations
   x <- na.omit(x)
   
-  # Construct the output string with:
-  # 1) The mean of 'x' rounded to 'k' decimal places
-  # 2) The symbol '±' followed by the standard deviation of 'x' rounded to 'k' decimals
-  # 3) Parentheses containing the range of 'x' values: minimum and maximum of 'x',
-  #    both rounded to 'k' decimal places
+  #' Construct the output string with:
+  #' 1) The mean of 'x' rounded to 'k' decimal places
+  #' 2) The symbol '±' followed by the standard deviation of 'x' rounded to 'k' decimals
+  #' 3) Parentheses containing the range of 'x' values: minimum and maximum of 'x',
+  #' both rounded to 'k' decimal places
   paste0(
     round(mean(x), k), "±",            # Mean ± symbol
     round(sd1(x), k), " (",            # Standard deviation
